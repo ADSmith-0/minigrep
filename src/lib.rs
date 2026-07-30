0a4eb5873f8a30c6c13d88361f52a6d0a5e8ef3e
ADSmith-0 <adam00199382818@gmail.com>
2026-07-30 22:30:19 +0100

    Added function body for search

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	vec![]
+  let mut matches = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      matches.push(line);
+    }
+  }
+
+  return matches;
 }
 
 #[cfg(test)]
